1bf223b3311836dcfcbb90d7f3d77f48fbec7ddc
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2025-03-08 23:07:34 +0530

    update mock data (#3)

---
+++ src/app/_components/file-row.tsx
@@ -0,0 +1,57 @@
+import type { File, Folder } from "@/lib/mock-data"
+
+import { FolderIcon, FileIcon, Upload, ChevronRight } from "lucide-react"
+import Link from "next/link"
+
+export const FileRow = ({ file }: { file: File }) => {
+  return (
+    <li
+      key={file.id}
+      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
+    >
+      <div className="grid grid-cols-12 items-center gap-4">
+        <div className="col-span-6 flex items-center">
+          <Link
+            href={file.url}
+            className="flex items-center text-gray-100 hover:text-blue-400"
+            target="_blank"
+          >
+            <FileIcon className="mr-3" size={20} />
+            {file.name}
+          </Link>
+        </div>
+        <div className="col-span-3 text-gray-400">file</div>
+        <div className="col-span-3 text-gray-400">{file.size}</div>
+      </div>
+    </li>
+  )
+}
+
+export const FolderRow = ({
+  folder,
+  handleFolderClick,
+}: {
+  folder: Folder
+  handleFolderClick: () => void
+}) => {
+  return (
+    <li
+      key={folder.id}
+      className="hover:bg-gray-750 border-b border-gray-700 px-6 py-4"
+    >
+      <div className="grid grid-cols-12 items-center gap-4">
+        <div className="col-span-6 flex items-center">
+          <button
+            onClick={() => handleFolderClick()}
+            className="flex items-center text-gray-100 hover:text-blue-400"
+          >
+            <FolderIcon className="mr-3" size={20} />
+            {folder.name}
+          </button>
+        </div>
+        <div className="col-span-3 text-gray-400"></div>
+        <div className="col-span-3 text-gray-400"></div>
+      </div>
+    </li>
+  )
+}
