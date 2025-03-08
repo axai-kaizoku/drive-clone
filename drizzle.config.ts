f39c251d6436650aab1222eee78e6e3a300d5a97
Akshay <119416860+axai-kaizoku@users.noreply.github.com>
2025-03-08 20:57:48 +0530

    add db setup (#2)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,9 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from "drizzle-kit"
 
-import { env } from "@/env";
+import { env } from "@/env"
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
   tablesFilter: ["drive-clone_*"],
-} satisfies Config;
+} satisfies Config
